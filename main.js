8db1f1d75fab33a157096d61ce7143e5d2912ccc
helenaKuthanova <helena.kuthanova@gmail.com>
2022-02-23 17:44:26 +0100

    panacek se pohybuje po stisknuti sipek

--- main.js
+++ main.js
@@ -7,3 +7,43 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
+let panacek = document.getElementById("panacek")
+let x = 400;
+let y = 300;
+let posunPanacka = 10
+
+// výchozí postavení
+panacek.style.left = x + "px";
+panacek.style.top = y + "px";
+
+// pohyb panáčka
+// zjistím key klávesy: ArrowDown, ArrowUp, ArrowRight, ArrowLeft
+function stiskKlavesy(udalost) {
+    kodKlavesy = udalost.key
+    console.log(kodKlavesy)
+
+    if (kodKlavesy === "ArrowDown") {
+        y += posunPanacka;
+        panacek.style.left = x + "px";
+        panacek.style.top = y + "px";
+    }
+
+    if (kodKlavesy === "ArrowUp") {
+        y -= posunPanacka;
+        panacek.style.left = x + "px";
+        panacek.style.top = y + "px";
+    }
+
+    if (kodKlavesy === "ArrowLeft") {
+        x -= posunPanacka;
+        panacek.style.left = x + "px";
+        panacek.style.top = y + "px";
+    }
+
+    if (kodKlavesy === "ArrowRight") {
+        x += posunPanacka;
+        panacek.style.left = x + "px";
+        panacek.style.top = y + "px";
+    }
+}
+
